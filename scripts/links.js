6b699d6c8874e11839944fa385aacae804e331f8
LindzMk <malindi1830@gmail.com>
2024-07-11 20:57:31 +0200

    added

--- scripts/links.js
+++ scripts/links.js
@@ -7,31 +7,32 @@ const getLinks = async () => {
     try {
         const response = await fetch(linksURL);
         const data = await response.json();
-        console.log(data);  // Test the JSON result
+        displayLinks(data);
     } catch (error) {
         console.error("Error fetching data: ", error);
     }
 };
 
 const displayLinks = (weeks) => {
-    weeks.forEach((week) => {
+    weeks.lessons.forEach((lesson) => {
+
         let weekTitle = document.createElement('h4');
-        weekTitle.textContent = week.week;
+        weekTitle.textContent = `Week ${lesson.lesson}`;
         links.appendChild(weekTitle);
 
-        week.links.forEach((link, index) => {
+        lesson.links.forEach((link, index) => {
             let linkURL = document.createElement('a');
             linkURL.textContent = link.title;
-            linkURL.href = link.url.startsWith('http') ? link.url : `${baseURL}${link.url}`;
+            linkURL.href = link.url;
             links.appendChild(linkURL);
 
-            if (index < week.links.length - 1) {
+
+            if (index < lesson.links.length - 1) {
                 let separator = document.createTextNode(" | ");
                 links.appendChild(separator);
             }
         });
-    });
+    })
 };
 
-// Call the getLinks function to fetch and display the data
 getLinks();
